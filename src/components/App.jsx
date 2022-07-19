7012a32a03c21ed34d59fe2d261a99fd6fe000a5
Salvador Salcedo <salsalcedo4321@gmail.com>
2022-07-19 11:25:24 -0700

    added and removed react-bootstrap bc it was interfering with tailwindcss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,18 @@ import Homepage from './Homepage';
 import Typeform from './Typeform';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return(
-    <div class="h-screen">
-      <Header />
-      <div class="flex flex-row w-full justify-center h-full">
-        <div class='flex flex-row w-5/6 h-full justify-center items-center '>
-          <h1>Calculate the Carbon footprint of...</h1>
+    return(
+        <div class="h-screen">
+            <Header />
+            <div class="flex flex-row w-full justify-center h-full">
+                <div class='flex flex-row w-5/6 h-full justify-center items-center '>
+                    <h1>Calculate the Carbon footprint of...</h1>
+                </div>
+            </div>
         </div>
-      </div>
-      
-    </div>
-  )
+    )
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,28 +3,21 @@ import React from 'react';
 // eslint-disable-next-line
 // import axios from 'axios';
 
-class Header extends React.Component {
-    constructor() {
-        super();
-
-}
-
-  render() {
-        return (
-            <div class="flex flex-row h-16 w-full justify-center bg-iris-blue text-[#FFFFFF]">
-                <div class="flex flex-row h-8 justify-between w-5/6 self-center">
-                    <h1 class="w-1/3 self-center font-bold">
-                        Hubble Labs Carbon Calculator
-                    </h1>
-                    <div className='flex flex-row justify-evenly w-1/2 self-center'>
-                        <a>Calculate</a>
-                        <a>How it Works</a>
-                        <a>Credit</a>
-                    </div>
+function Header() {
+	return(
+		<div class="flex flex-row h-16 w-full justify-center bg-iris-blue text-[#FFFFFF]">
+            <div class="flex flex-row h-8 justify-between w-5/6 self-center">
+                <h1 class="w-1/3 self-center font-bold">
+                    Hubble Labs Carbon Calculator
+                </h1>
+                <div className='flex flex-row justify-evenly w-1/2 self-center'>
+                    <a>Calculate</a>
+                    <a>How it Works</a>
+                    <a>Credit</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+	)
 }
 
 export default Header;
