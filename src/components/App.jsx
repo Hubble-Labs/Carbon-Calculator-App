4e6fcd1f3d6deefd1f053723a4362969b2e1cdc5
Salvador Salcedo <salsalcedo4321@gmail.com>
2022-07-18 23:24:05 -0700

    idk whats going on

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,21 @@
 //This file will be the root of our application
-import React from 'react';
+import React, { useState } from 'react';
 // import axios from 'axios';
 import Header from './Header'
 
-class App extends React.Component {
-    constructor() {
-        super();
+function App() {
+  const [count, setCount] = useState(0);
 
-  }
-
-  render () {
-      return(
-        <div container mx-auto >
-            <div class="flex flex-row w-full justify-center">
-              <Header />
-                <div class='w-5/6'>
-                  <h1>Calculate the Carbon footprint of...</h1>
-                </div>
-            </div>
-        </div>
-    )
-  }
+  return(
+    <div container mx-auto >
+      <div class="flex flex-row w-full justify-center">
+        <Header />
+          <div class='w-5/6'>
+            <h1>Calculate the Carbon footprint of...</h1>
+          </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command, mode }) => {
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-        root: 'src',
-        publicDir: '../public',
-        build: {
-          // Relative to the root
-          outDir: '../dist',
-        },
-        plugins: [
-            react({
-                // Use React plugin in all *.jsx and *.tsx files
-                include: '**/*.{jsx,tsx}',
-            })
-        ],
-        define: {
-            __APP_ENV__: env.APP_ENV
-        }
-    }
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()]
 })
+
