c01bd82660404c50a0453f174046d3dfa50e70af
Salvador Salcedo <salsalcedo4321@gmail.com>
2022-07-18 19:47:44 -0700

    fixed dev vite maybe

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
-  build: {
-    // Relative to the root
-    outDir: '../dist',
-  },
-  plugins: [
-    createHtmlPlugin({
-      inject: {
-        data: {
-          title: env === 'production' ? 'My site' : `My site [${env.toUpperCase()}]`,
+export default defineConfig(({ command, mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        root: 'src',
+        publicDir: '../public',
+        build: {
+          // Relative to the root
+          outDir: '../dist',
         },
-      },
-    }),
-    react({
-        // Use React plugin in all *.jsx and *.tsx files
-        include: '**/*.{jsx,tsx}',
-    })
-  ],
-});
+        plugins: [
+            react({
+                // Use React plugin in all *.jsx and *.tsx files
+                include: '**/*.{jsx,tsx}',
+            })
+        ],
+        define: {
+            __APP_ENV__: env.APP_ENV
+        }
+    }
+})
